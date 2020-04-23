d31bbcc5edcf65ec3a9cf9d73a01b24b3224fa0d
LasithNiro <lasith2013.l2n@gmail.com>
2020-04-23 18:24:02 +0100

    Added about animation

---
+++ Assets/Scripts/AnimateCredits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimateCredits : MonoBehaviour
+{
+    public GameObject credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RunCredits());
+    }
+    IEnumerator RunCredits()
+    {
+        yield return new WaitForSeconds(0.5f);
+        credits.SetActive(true);
+        yield return new WaitForSeconds(15);
+        SceneManager.LoadScene("Menu2");
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
 
     public void showDetails()
     {
-
+        SceneManager.LoadScene("About");
     }
 
     public void QuitGame()
